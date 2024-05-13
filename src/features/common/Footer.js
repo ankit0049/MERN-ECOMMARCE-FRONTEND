3a08e24d8c1b46f01a816d019fe7089d5e564e7c
ankit_049 <ankitrajputji00@gmail.com>
2024-05-14 01:48:14 +0530

    NodeMailer Integrate Typos Fix and hardcoded API remove

--- src/features/common/Footer.js
+++ src/features/common/Footer.js
@@ -1,23 +1,23 @@
 function Footer() {
   return (
     <>
-      <div className=" bg-gray-900">
+      <div className=" bg-gray-900 h-40 md:h-100">
         <div className="max-w-2xl mx-auto text-white py-10">
           <div className="text-center">
-            <h3 className="text-3xl mb-3"> Download our E-Bazar App </h3>
-            <p> Buy what you want. </p>
+            <h3 className="text-3xl mb-3"> Download our Ecommerce App</h3>
+            <p> Buy what you want. and Don't Get Your Product 😂 Just Kidding ! </p>
             <div className="flex justify-center my-10">
-              <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
+              <div className="flex items-center border  rounded-lg px-4 py-2 w-52 mx-2">
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
                   className="w-7 md:w-8"
                 />
                 <div className="text-left ml-3">
                   <p className="text-xs text-gray-200">Download on </p>
-                  <p className="text-sm md:text-base"> Google Play Store </p>
+                  <p className="text-sm md:text-base"> Ankit  Store 😁  </p>
                 </div>
               </div>
-              <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2">
+              <div className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
                   className="w-7 md:w-8"
@@ -32,7 +32,7 @@ function Footer() {
           <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
             <p className="order-2 md:order-1 mt-8 md:mt-0">
               {' '}
-              © Ankit Rajput, 2023.{' '}
+              © Ankit Rajput, 2024.{' '}
             </p>
             <div className="order-1 md:order-2">
               <span className="px-2">About us</span>
